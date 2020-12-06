421dcf145c5993fa038d6fb88a77ed38f16d66fd
unknown <Mahdi.Aslami@PART.LOCAL>
2020-12-06 11:55:04 +0330

    Fix printing output.

--- Simple/FastRoute/Exceptions/MethodNotAllowedException.php
+++ Simple/FastRoute/Exceptions/MethodNotAllowedException.php
@@ -6,6 +6,6 @@ class MethodNotAllowedException extends HttpException
 {
     public static function createWithMessage($method, $path)
     {
-        return new self(printf('`%s` method not allowd for `%s` path.', $method, $path), 405);
+        return new self(sprintf('`%s` method not allowd for `%s` path.', $method, $path), 405);
     }
 }
--- Simple/FastRoute/Exceptions/NotFoundException.php
+++ Simple/FastRoute/Exceptions/NotFoundException.php
@@ -6,6 +6,6 @@ class NotFoundException extends HttpException
 {
     public static function createWithMessage($path)
     {
-        return new self(printf('`%s` Not found.', $path), 404);
+        return new self(sprintf('`%s` Not found.', $path), 404);
     }
 }
