6c731042d7bc9bfc13f221e2e44262a28988cf9a
unknown <Mahdi.Aslami@PART.LOCAL>
2020-12-06 17:52:03 +0330

    Integrate helpers.

--- Simple/helpers.php
+++ Simple/helpers.php
@@ -36,3 +36,27 @@ if (!function_exists('base_path')) {
         return base_path($path);
     }
 }
+
+if (!function_exists('env')) {
+    /**
+     * Get or set environment variable
+     * 
+     * For setting a value send both $key and $value parameters
+     * 
+     * For getting a value only send $key parameter.
+     * 
+     * @param string $key
+     * @param mixed $value
+     * @return mixed
+     */
+    function env($key, $value = null)
+    {
+        $key = strtoupper($key);
+
+        if ($value) {
+            $_ENV[$key] = $value;
+        }
+
+        return $_ENV[$key];
+    }
+}
