514a41c113b6c030f413046efd45d5774da4d574
mahdyaslami <mahdyaslami@gmail.com>
2020-12-07 09:21:51 +0330

    Fit doc.

--- src/helpers.php
+++ src/helpers.php
@@ -33,8 +33,8 @@ if (!function_exists('env')) {
      * 
      * For getting a value only send $key parameter.
      * 
-     * @param string $key
-     * @param mixed $value
+     * @param  string  $key
+     * @param  mixed   $value
      * @return mixed
      */
     function env($key, $value = null)
