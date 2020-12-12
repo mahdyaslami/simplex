b21d79fa346a6ea9913951fee16398624863fbc9
Mahdi Aslami <mahdyaslami@gmail.com>
2020-12-12 22:27:56 +0330

    Fix conflict.

--- src/FastRoute/Exceptions/MethodNotAllowedException.php
+++ src/FastRoute/Exceptions/MethodNotAllowedException.php
@@ -2,10 +2,10 @@
 
 namespace Simple\FastRoute\Exceptions;
 
-class MethodNotAllowedException extends HttpException
+class MethodNotAllowedException extends HttpException 
 {
-    public static function createWithMessage($method, $path)
-    {
+  public static function createWithMessage($method, $path)
+  {
         return new self(sprintf('`%s` method not allowd for `%s` path.', $method, $path), 405);
-    }
+  }
 }
--- src/FastRoute/Exceptions/NotFoundException.php
+++ src/FastRoute/Exceptions/NotFoundException.php
@@ -2,10 +2,10 @@
 
 namespace Simple\FastRoute\Exceptions;
 
-class NotFoundException extends HttpException
+class NotFoundException extends HttpException 
 {
-    public static function createWithMessage($path)
-    {
+  public static function createWithMessage($path)
+  {
         return new self(sprintf('`%s` Not found.', $path), 404);
-    }
+  }
 }
