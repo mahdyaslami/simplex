490de035c60e11398e47e02bda7d458f795b5830
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2021-03-27 14:20:26 +0430

    Remove unused method.

--- src/helpers.php
+++ src/helpers.php
@@ -2,18 +2,6 @@
 
 if (!function_exists('base_path')) {
     /**
-     * Determine if the application is running in the console.
-     *
-     * @return bool
-     */
-    function running_in_console()
-    {
-        return \PHP_SAPI === 'cli' || \PHP_SAPI === 'phpdbg';
-    }
-}
-
-if (!function_exists('base_path')) {
-    /**
      * Get the path to the base of the working directory.
      *
      * @param  string  $path
