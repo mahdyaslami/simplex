360c1be8e4718e3a4055c6aa8c81fa9f4f09a3f3
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2021-03-27 12:41:20 +0430

    Test extending http exception.

--- tests/fastroute/ExceptionsTest.php
+++ tests/fastroute/ExceptionsTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Simple\FastRoute\Exceptions\HttpException;
 use Simple\FastRoute\Exceptions\MethodNotAllowedException;
 use Simple\FastRoute\Exceptions\NotFoundException;
 use Simple\Test\TestCase;
@@ -20,6 +21,18 @@ final class ExceptionsTest extends TestCase
 
     /**
      * @test
+     * @covers \Simple\FastRoute\Exceptions\MethodNotAllowedException
+     * @uses \Simple\Test\TestCase
+     */
+    public function method_not_allowed_extended_http_exception()
+    {
+        $exception = MethodNotAllowedException::createWithMessage('METHOD', 'PATH');
+
+        $this->assertInstanceOf(HttpException::class, $exception, 'Method not allowed exception should extend HttpException.');
+    }
+
+    /**
+     * @test
      * @covers \Simple\FastRoute\Exceptions\NotFoundException
      * @uses \Simple\Test\TestCase
      */
@@ -28,5 +41,17 @@ final class ExceptionsTest extends TestCase
         $exception = NotFoundException::createWithMessage('PATH');
 
         $this->assertEquals('`PATH` Not found.', $exception->getMessage(), 'Message is not correct.');
+    }
+
+    /**
+     * @test
+     * @covers \Simple\FastRoute\Exceptions\NotFoundException
+     * @uses \Simple\Test\TestCase
+     */
+    public function not_found_extended_http_exception()
+    {
+        $exception = NotFoundException::createWithMessage('PATH');
+
+        $this->assertInstanceOf(HttpException::class, $exception, 'Not found exception should extend HttpException.');
     }
 }
