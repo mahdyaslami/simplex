2105a139c9d4dd13bee3d1676bc535dbd8e165c8
Mahdi Aslami <mahdyaslami@gmail.com>
2021-04-03 20:22:24 +0430

    Test TestCase class.

---
+++ tests/Test/TestCaseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SimpleTests\Test\TestCaseTest;
+
+use Simple\Test\TestCase;
+
+final class TestCaseTest extends TestCase
+{
+    /**
+     * @test
+     * @covers \Simple\Test\TestCase
+     */
+    public function check_get_fill_request_uri_and_request_method()
+    {
+        $this->get('/test');
+
+        $this->assertEquals('GET', $_SERVER['REQUEST_METHOD']);
+        $this->assertEquals('/test', $_SERVER['REQUEST_URI']);
+    }
+
+    /**
+     * @test
+     * @covers \Simple\Test\TestCase
+     */
+    public function check_post_fill_request_uri_and_request_method()
+    {
+        $this->post('/test');
+
+        $this->assertEquals('POST', $_SERVER['REQUEST_METHOD']);
+        $this->assertEquals('/test', $_SERVER['REQUEST_URI']);
+    }
+}
