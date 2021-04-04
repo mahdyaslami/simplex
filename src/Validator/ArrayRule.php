addd3bfc71d53abc057e3b1982084a65b9ae13c5
Mahdi Aslami <mahdyaslami@gmail.com>
2021-04-04 22:11:25 +0430

    Remove unused code.

--- src/Validator/ArrayRule.php
+++ src/Validator/ArrayRule.php
@@ -6,21 +6,8 @@ use Simple\Validator\Contracts\Rule;
 
 class ArrayRule extends Rule
 {
-    private Rule $validator;
-
-    public function __construct(Rule $validator)
-    {
-        $this->validator = $validator;
-    }
-
     public function validate($items)
     {
-        $this->emtpyErrors();
-
-        foreach ($items as $item) {
-            $this->validator->validate($item);
-
-            $this->addErrors($this->validator->errors());
-        }
+        //
     }
 }
--- src/Validator/ObjectRule.php
+++ src/Validator/ObjectRule.php
@@ -6,21 +6,8 @@ use Simple\Validator\Contracts\Rule;
 
 class ObjectRule extends Rule
 {
-    private array $rules;
-
-    public function __construct(array $rules)
-    {
-        $this->rules = $rules;
-    }
-
     public function validate($object)
     {
-        $this->emtpyErrors();
-
-        foreach ($this->rules as $rule) {
-            $rule->validate($object);
-
-            $this->addErrors($rule->errors);
-        }
+        //
     }
 }
