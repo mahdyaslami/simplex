fb0fe23e2b3a92db28ce9bc5be141544cbe8e30c
Mahdi Aslami <mahdyaslami@gmail.com>
2021-04-10 21:09:27 +0430

    Cover exceptions.

--- tests/Validator/Exceptions/ValidationExceptionTest.php
+++ tests/Validator/Exceptions/ValidationExceptionTest.php
@@ -3,6 +3,9 @@
 namespace SimpleTests\Validator\Exceptions;
 
 use Simple\Test\TestCase;
+use Simple\Validator\Exceptions\GreaterNumberNotAllowedException;
+use Simple\Validator\Exceptions\OnlyIntegerAllowedException;
+use Simple\Validator\Exceptions\OnlyNumberAllowedException;
 use Simple\Validator\Exceptions\ValidationException;
 
 final class ValidationExceptionTest extends TestCase
@@ -20,5 +23,38 @@ final class ValidationExceptionTest extends TestCase
 
         $this->assertEquals('first', $errors[0]);
         $this->assertEquals('second', $errors[1]);
+    }
+
+    /**
+     * @test
+     * @covers \Simple\Validator\Exceptions\OnlyNumberAllowedException
+     */
+    public function only_number_allowed()
+    {
+        $exception = new OnlyNumberAllowedException();
+
+        $this->assertEquals('Value must be number.', $exception->getMessage());
+    }
+
+    /**
+     * @test
+     * @covers \Simple\Validator\Exceptions\OnlyIntegerAllowedException
+     */
+    public function only_integer_allowed()
+    {
+        $exception = new OnlyIntegerAllowedException();
+
+        $this->assertEquals('Value must be integer.', $exception->getMessage());
+    }
+
+    /**
+     * @test
+     * @covers \Simple\Validator\Exceptions\GreaterNumberNotAllowedException
+     */
+    public function greater_number_not_allowed()
+    {
+        $exception = new GreaterNumberNotAllowedException(10);
+
+        $this->assertEquals('Value must be lower than 10.', $exception->getMessage());
     }
 }
