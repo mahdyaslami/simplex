dedad3c7e00615a234d3219ab0711fa32b8b7f5c
Mahdi Aslami <mahdyaslami@gmail.com>
2021-04-18 17:43:45 +0430

    Add app interface and class.

---
+++ src/Contracts/ApplicationInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Simplex\Contracts;
+
+interface ApplicationInterface
+{
+    public function run();
+}
---
+++ src/Http/Application.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Simplex\Http;
+
+use Simplex\Contracts\ApplicationInterface;
+
+class Application implements ApplicationInterface
+{
+    public function run()
+    {
+        //
+    }
+}
