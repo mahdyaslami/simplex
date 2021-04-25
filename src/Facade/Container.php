2a5179dabde55364b88454b8c7d5abc506fc1e10
Mahdi Aslami <mahdyaslami@gmail.com>
2021-04-25 18:53:34 +0430

    Add container facade.

---
+++ src/Facade/Container.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Simplex\Facade;
+
+use League\Container\Container as LeagueContainer;
+
+class Container
+{
+    private $self = null;
+
+    private $container;
+
+    private function __construct()
+    {
+        $this->container = new LeagueContainer();
+    }
+
+    /**
+     * Add an item to the container.
+     * 
+     * @param string $id
+     * @param mixed  $concrete
+     * @return void
+     */
+    public function bind(string $id, $concrete)
+    {
+        $this->container->add($id, $concrete);
+    }
+
+    /**
+     * Add an item to the container. the item is always unique.
+     * 
+     * @param  string $id
+     * @param  mixed  $concrete
+     * @return void
+     */
+    public function singleton(string $id, $concrete)
+    {
+        $this->container->add($id, $concrete, true);
+    }
+
+    /**
+     * Return true if container can return object for identifier.
+     * 
+     * @param string $id — Identifier of the entry to look for.
+     * @return bool
+     */
+    public function has(string $id)
+    {
+        return $this->container->has($id);
+    }
+
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     * 
+     * @param  string $id — Identifier of the entry to look for.
+     * @return mixed
+     */
+    public function get(string $id)
+    {
+        return $this->container->get($id);
+    }
+
+    /**
+     * Get instanse of self.
+     * 
+     * @return $this
+     */
+    public function getInstance()
+    {
+        if (is_null($this->self)) {
+            $this->self = new static;
+        }
+
+        return $this->self;
+    }
+}
